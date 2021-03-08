7b22502fa69540aac0b4c68ed0a56d2876345005
fronte-finem <fronte.finem+github@ya.ru>
2021-03-08 19:21:44 +0300

    feat: task "Carbon Dating" pass tests

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(str) {
+  if (typeof str != 'string') return false;
+  const N = parseFloat(str);
+  if (isNaN(N) || N <= 0 || N > MODERN_ACTIVITY) return false;
+  const k = Math.log(2) / HALF_LIFE_PERIOD;
+  return Math.ceil( Math.log(MODERN_ACTIVITY / N) / k );
 };
