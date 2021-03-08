0c2d21b310b12257ce9a55da59e8a00a4a1cfdf1
fronte-finem <fronte.finem+github@ya.ru>
2021-03-08 21:33:07 +0300

    feat: task "Extended repeater" pass tests

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+const DEFAULT_SEPARATOR_LVL_1 = '+';
+const DEFAULT_SEPARATOR_LVL_2 = '|';
+
+module.exports = function repeater(str, options) {
+  if (options.addition !== undefined) {
+    str += repeat(options.addition
+                , options.additionRepeatTimes || 0
+                , options.additionSeparator || DEFAULT_SEPARATOR_LVL_2);
+  }
+  return repeat(str
+              , options.repeatTimes || 0
+              , options.separator || DEFAULT_SEPARATOR_LVL_1);
 };
-  
+
+function repeat(any, times, sep) {
+  str = String(any);
+  let res = [str];
+  for (let i=1; i < times; i++) {
+    res.push(sep);
+    res.push(str)
+  }
+  return res.join('');
+};
