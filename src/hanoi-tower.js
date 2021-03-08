53f09764e1f7b4c9b8fc93ed3dfac3515c5057d8
fronte-finem <fronte.finem+github@ya.ru>
2021-03-08 22:07:57 +0300

    feat: task "Tower of Hanoi" pass tests

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+const SECONDS_IN_HOUR = 3600;
+
+const hanoiMinTurns = (disks) => 2**disks - 1;
+
+module.exports = function calculateHanoi(disks, speed) {
+  let turns = hanoiMinTurns(disks);
+  return {
+    turns: turns,
+    seconds: Math.floor(SECONDS_IN_HOUR / speed * turns)
+  }
+}
