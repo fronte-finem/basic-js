8a867b4b36c4559198a07f647fb2c18c0e63029b
fronte-finem <fronte.finem+github@ya.ru>
2021-03-08 23:49:18 +0300

    feat: task "Chain maker" pass tests

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,24 +2,45 @@ const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this._chain.length;
   },
+
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (!this._chain) this._chain = [];
+    this._chain.push(String(value));
+    return this;
   },
+
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (!this._chain) {
+      throw new Error('Chain is empty!');
+    }
+    if (!Number.isInteger(position)) {
+      this._destroy();
+      throw new Error('Given position is not the integer!');
+    }
+    if (position < 1 || position > this.getLength()) {
+      this._destroy();
+      throw new Error('Given position out of range!');
+    }
+    this._chain.splice(position - 1, 1);
+    return this;
   },
+
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (!this._chain) return this;
+    this._chain.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let fancyChain = this._chain.map(x => `( ${x} )`).join('~~');
+    this._destroy();
+    return fancyChain;
+  },
+
+  _destroy() {
+    this._chain = undefined;
   }
 };
 
