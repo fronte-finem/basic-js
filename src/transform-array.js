f1d5aeb68df1ca8442af9a58c952c55a6b017eeb
fronte-finem <fronte.finem+github@ya.ru>
2021-03-09 17:58:54 +0300

    feat: task "Transform array" pass tests

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+const NOT_NEXT = '--discard-next';
+const NOT_PREV = '--discard-prev';
+const X2_NEXT = '--double-next';
+const X2_PREV = '--double-prev';
+
+const notDiscarded = (arr, i) => i >= 0 && i < arr.length && arr[i] !== NOT_NEXT;
+
+module.exports = function transform(arr) {
+  let res = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case (NOT_NEXT):
+        i++;
+        break;
+      case (X2_NEXT):
+        if (i+1 < arr.length) res.push(arr[i+1]);
+        break;
+      case (NOT_PREV):
+        if (notDiscarded(arr, i-2)) res.pop();
+        break;
+      case (X2_PREV):
+        if (notDiscarded(arr, i-2)) res.push(arr[i-1]);
+        break;
+      default:
+        res.push(arr[i]);
+    }
+  }
+
+  return res;
 };
