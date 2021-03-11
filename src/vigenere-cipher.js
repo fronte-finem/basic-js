48ab958ee4a1af41d83a1f978e02816a9f40b379
fronte-finem <fronte.finem+github@ya.ru>
2021-03-11 22:04:07 +0300

    refactor: clean functional dublication

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -10,18 +10,14 @@ const latinFiniteMonoid = (x, i) => String.fromCharCode((x.charCodeAt() + i - _A
 
 const LATIN = Array(LATIN_AMOUNT).fill('A').map(latinFiniteMonoid);
 
-const toEncMap = (getVal, xs) => xs.reduce((o,x) => {o[x] = getVal(x); return o}, {});
-const toDecMap = (getVal, xs) => xs.reduce((o,x) => {o[getVal(x)] = x; return o}, {});
-
-const EncTabulaRecta = LATIN.reduce((acc, x, i) => {
-  acc[x] = toEncMap(x => latinFiniteMonoid(x,i), LATIN);
+const genTabulaRecta = (record) => LATIN.reduce((acc, x, i) => {
+  acc[x] = LATIN.reduce((o,x) => record(o,x,i), {});
   return acc
 }, {});
 
-const DecTabulaRecta = LATIN.reduce((acc, x, i) => {
-  acc[x] = toDecMap(x => latinFiniteMonoid(x,i), LATIN);
-  return acc
-}, {});
+const EncTabulaRecta = genTabulaRecta((o,x,i) => { o[x] = latinFiniteMonoid(x,i); return o });
+const DecTabulaRecta = genTabulaRecta((o,x,i) => { o[latinFiniteMonoid(x,i)] = x; return o });
+
 
 function* repeat(xs) { while(true) for (let x of xs) yield x; }
 
@@ -30,24 +26,17 @@ class VigenereCipheringMachine {
     this.direct = direct;
   }
 
-  encrypt(msg, key) {
-    let res = this.enc(msg, key);
-    return this.direct ? res.join('') : res.reverse().join('');
-  }
-  decrypt(msg, key) {
-    let res = this.dec(msg, key);
-    return this.direct ? res.join('') : res.reverse().join('');
-  }
-
-  enc(msg, key) { return this.process(msg, key, EncTabulaRecta); }
-  dec(msg, key) { return this.process(msg, key, DecTabulaRecta); }
+  encrypt(msg, key) { return this.process(msg, key, EncTabulaRecta); }
+  decrypt(msg, key) { return this.process(msg, key, DecTabulaRecta); }
 
   process(msg, key, tabulaRecta) {
     this.validate(msg, key);
     msg = msg.toUpperCase();
     const genKey = repeat(key.toUpperCase());
 
-    return msg.split('').map(x => x < 'A' || x > 'Z' ? x : tabulaRecta[genKey.next().value][x]);
+    let res = msg.split('').map(x => x < 'A' || x > 'Z' ? x : tabulaRecta[genKey.next().value][x]);
+
+    return this.direct ? res.join('') : res.reverse().join('');
   }
 
   validate(msg, key) {
