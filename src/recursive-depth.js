e5bedaf006464e67dbfb87918bc23660ed9527ff
fronte-finem <fronte.finem+github@ya.ru>
2021-03-11 21:46:18 +0300

    refactor: simplify function pass

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,7 +2,7 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
   calculateDepth(arr) {
-    let deep = arr.filter(x => Array.isArray(x));
+    let deep = arr.filter(Array.isArray);
     if (deep.length == 0) return 1;
     return 1 + Math.max(...deep.map(this.calculateDepth.bind(this)));
   }
